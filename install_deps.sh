5eefede5e2fbf6b39333d66506ca950862f7634f
cnb <cneirabustos@gmail.com>
2017-03-15 01:53:12 -0300

    fix deps

--- install_deps.sh
+++ install_deps.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 set -ex
-git clone https://github.com/oktal/pistache.git
-cd pistache
-git submodule update --init
-mkdir build
-cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
-make
-sudo make install
-cd
 git clone  https://github.com/miloyip/rapidjson.git
 cd rapidjson
 git submodule update --init
@@ -17,3 +8,14 @@ cd build
 cmake  ..
 make
 sudo make install
+cd
+git clone https://github.com/oktal/pistache.git
+cd pistache
+git submodule update --init
+mkdir build
+cd build
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+make
+sudo make install
+
+
