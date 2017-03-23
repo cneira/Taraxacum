c199aff4e1406960ed5240a1003bb4b8b9c7a558
Carlos Neira <cneirabustos@gmail.com>
2017-03-23 21:11:11 +0000

    change deps from pistache to local pistache fixes

--- install_deps.sh
+++ install_deps.sh
@@ -2,12 +2,14 @@
 set -ex
 git clone  https://github.com/miloyip/rapidjson.git
 cd rapidjson
-cd include
-sudo cp -rp rapidjson  /usr/include/
-cd
-git clone https://github.com/oktal/pistache.git
-cd pistache
 git submodule update --init
+mkdir build
+cd build
+cmake ..
+sudo make install
+cd
+git clone https://github.com/cneira/pistache
+cd pistache
 mkdir build
 cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
